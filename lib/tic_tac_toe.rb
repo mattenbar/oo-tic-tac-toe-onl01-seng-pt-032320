650076056326fc9a7039a23668b52c44e3cbdb26
matthew enbar <matthew.enbar@gmail.com>
2020-04-03 20:46:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -88,9 +88,9 @@ class TicTacToe
     position_3 = @board[index_2]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
+      return combo
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
+      return combo
     end
   }
   return false
